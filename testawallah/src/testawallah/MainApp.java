f7523fe3cb8e235a0fe1bd08f64df8d91825eb68
Dr. Ray Klump <klumpra@lewisu.edu>
2019-10-20 08:38:35 -0500

    Merged branches

--- testawallah/src/testawallah/MainApp.java
+++ testawallah/src/testawallah/MainApp.java
@@ -4,6 +4,6 @@ public class MainApp {
   public static void main(String[] args) {
     System.out.println("Hey");
     System.out.println("Another change");
-    System.out.println("Testing this.");
+    System.out.println("Testing this."); 
   }
 }
